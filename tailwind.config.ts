e6e8f73b95b835c593fee6668c98c97f4f12a9f7
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2023-09-26 23:29:32 +0000

    change the fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type {Config} from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -8,13 +8,27 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        Poppins: ['var(--font-Poppins)'],
+        Josefin: ['var(--font-Josefin)'],
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      screens: {
+        '1000px': '1000px',
+        '1100px': '1100px',
+        '1200px': '1200px',
+        '1300px': '1300px',
+        '1400px': '1400px',
+        '1500px': '1500px',
+        '800px': '800px',
+        '400px': '400px',
+      },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
