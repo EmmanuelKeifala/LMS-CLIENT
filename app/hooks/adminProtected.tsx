416fe574e42c5ae73237b4b86192e41688fa64e8
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2023-09-29 14:13:00 +0000

    created an logic for admin protected routes

---
+++ app/hooks/adminProtected.tsx
@@ -0,0 +1,13 @@
+import {redirect} from 'next/navigation';
+import UserAuth from './userAuth';
+import {useSelector} from 'react-redux';
+interface ProtectedProps {
+  children: React.ReactNode;
+}
+export default function AdminProtected({children}: ProtectedProps) {
+  const {user} = useSelector((state: any) => state.auth);
+
+  const isAdmin = user?.role === 'admin';
+
+  return isAdmin ? children : redirect('/');
+}
